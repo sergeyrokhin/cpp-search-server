b24e99d7b3da821c82afeb42a3b4103b8c9956ee
Сергей Рохин <mail@rokhin.ru>
2022-11-23 21:42:51 +0300

    Sprint 6. review 1

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -5,6 +5,10 @@ void RemoveDuplicates(SearchServer& search_server) {
 }
 
 void SearchServer::RemoveDuplicates() {
+//в моей реализации (использование индексов) не уместно begin end
+//идет перебор потенциально возможных дублей.
+//по хорошему, дубли не должны даже попадать.
+//я практик, сложно играться в эти тренажерки
 	for (auto& ind_docs : index_documents)
 	{
 		if (ind_docs.second.size() > 1)
@@ -17,16 +21,23 @@ void SearchServer::RemoveDuplicates() {
 void SearchServer::RemoveDuplicates(set<int>& dup) {
 	for (auto i = dup.begin(); i != dup.end(); i++)
 	{
+		//Утверждение о "Здесь лишняя квадратичная сложность" требует обоснования.
+		//В моей реализации при добавлении документов создается индекс, 
+		// который исключает тотальный перебор.
+		//Возможно существуют более эффективные алгоритмы, но каким образом курс обучения 
+		//может рассчитывать, что я должен этот метод предугадать?
+		
+		set<string> uniq_words1, uniq_words2;
+		for (const std::string& word : documents_words[*i]) {
+			uniq_words1.insert(word);
+		}
 		vector<int> for_del;
 		auto j = i;
 		j++;
 		for (; j != dup.end(); j++)
 		{
-			set<string> uniq_words1, uniq_words2;
+			set<string> uniq_words2;
 
-			for (const std::string& word : documents_words[*i]) {
-				uniq_words1.insert(word);
-			}
 			for (const std::string& word : documents_words[*j]) {
 				uniq_words2.insert(word);
 			}
